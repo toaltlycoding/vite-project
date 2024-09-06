99d9aa69df483aaaa51412f812988d220326cb54
Heather Toal <htoal@balanceinnovations.com>
2024-09-06 10:24:23 -0500

    Added full width container

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { FullWidthContainer } from "./components/FullWidthContainer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div>
-      test
-    </div>
+    <FullWidthContainer>
+     test
+    </FullWidthContainer>
   )
 }
 
